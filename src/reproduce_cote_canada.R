c88d79bbfab8b5c732220f26ac22f6d411c8fc89
Christopher Blier-Wong <christopher.blierwong@gmail.com>
2019-03-07 18:05:22 -0500

    ajout modeles tarifaires

--- src/reproduce_cote_canada.R
+++ src/reproduce_cote_canada.R
@@ -1,9 +1,9 @@
-###################################
-# Title : reproduce_cote          #
-# Goal : Replicate mémoire (2016) #
-# Author : Christopher Blier-Wong #
-# Date : January 2019             #
-###################################
+######################################################
+# Title : reproduce_cote                             #
+# Goal : Replicate mémoire (2016) with canadian data #
+# Author : Christopher Blier-Wong                    #
+# Date : January 2019                                #
+######################################################
 
 # Library and source functions --------------------------------------------
 
@@ -14,21 +14,21 @@ library(mgcv)
 
 # Import data -------------------------------------------------------------
 
-data_original <- read_csv("src/data/Spain-2011.csv")
+data_original <- read_csv("src/data/EARNIX_INPUT_2016_12_16.csv")
 
 
 # Rename variables --------------------------------------------------------
 
-data <- data_original %>% 
-  mutate(nb1 = N_mat_ase, nb2 = N_mat_con, nb3 = N_per_ase, nb4 = N_per_con) %>% 
-  mutate(km = km_anuales) %>% 
-  mutate(d = expo / 365) %>% 
-  mutate(age = 2011 - as.numeric(substr(fec_con, 1, 4))) %>% 
-  mutate(ageveh = 2011 - as.numeric(substr(fec_mat, 1, 4))) %>% 
-  mutate(sexe = 1 * (sexo_con == "HOMBRE")) %>% 
-  mutate(stn = 1 * (garaje == "Garaje privado")) %>% 
-  select(c(km, d, age, ageveh, sexe, stn, nb2))
+names(data_original)
 
+data <- data_original %>% 
+  mutate(nb2 = RA_NB_CLAIM) %>% 
+  mutate(km = RA_DISTANCE_DRIVEN) %>% 
+  mutate(age = RA_DRIVERAGE) %>% 
+  mutate(sexe = RA_GENDER) %>% 
+  mutate(d = RA_exposure_time) %>% 
+  mutate(ageveh = RA_VEHICLE_AGE) %>% 
+  select(c(km, d, age, ageveh, sexe, nb2, RA_PARTITION))
 
 # Reproduce section 3.2 ---------------------------------------------------
 
@@ -63,39 +63,19 @@ data %>% select(age) %>% summary
 data %>% select(ageveh) %>% summary
 
 # Tableau 3.4
-data %>% select(sexe) %>% as.matrix %>% mean
-data %>% select(stn) %>% as.matrix %>% mean
+data %>% select(sexe) %>% table
 
 
 # Reproduce section 3.3 ---------------------------------------------------
 
 # Rename variables
 
-data <- data_original %>% 
-  mutate(nb1 = N_mat_ase, nb2 = N_mat_con, nb3 = N_per_ase, nb4 = N_per_con) %>% 
-  mutate(km = km_anuales) %>% 
-  mutate(d = expo / 365) %>% 
-  mutate(age = 2011 - as.numeric(substr(fec_con, 1, 4))) %>% 
-  mutate(ageveh = 2011 - as.numeric(substr(fec_mat, 1, 4))) %>% 
-  mutate(sexe = 1 * (sexo_con == "HOMBRE")) %>% 
-  mutate(stn = 1 * (garaje == "Garaje privado")) %>% 
-  dplyr::select(c(km, d, age, ageveh, sexe, stn, nb2)) %>% 
-  filter(nb2 < km)
-
+data <- data %>% mutate(sexe == "MALE")
+  
 # Seperate dataset 
 
-set.seed(23)
-
-test_index <- sort(sample(1:nrow(data), 5000))
-
-data_train <- data[-test_index, ]
-data_test <- data[test_index, ]
-
-# Different que Steven ici car je sous-echantillonne sur les observations
-# retenues et non toutes les observations. Pour cette raison, 55862 donnees
-# sont differentes. Ainsi, ce decallage cause que 4293/66486 des observations 
-# ne sont pas equivalantes.
-
+data_test <- data %>% filter(RA_PARTITION == "TEST") %>% select(-RA_PARTITION)
+data_train <- data %>% filter(RA_PARTITION == "TRAIN") %>% select(-RA_PARTITION)
 
 # Fit model 3.1 -----------------------------------------------------------
 
@@ -129,7 +109,7 @@ data_model3.3 <- data_train %>% select(nb2, km, d, age) %>%
   mutate(x6 = age <= 25) %>% 
   mutate(x7 = (age > 25 & age <= 30))
 
-glm3.out <- glm(nb2 ~ 1 + x1 + x2 + x3 + x4 + x5 + offset(log(d)), 
+glm3.out <- glm(nb2 ~ 1 + x1 + x2 + x3 + x4 + x5 + offset(I(log(d))), 
                 family = poisson(link = log), data = data_model3.3)
 
 summary(glm3.out)
@@ -141,7 +121,7 @@ data_train <- data_train %>% mutate(x6 = age <= 25) %>%
   mutate(x7 = (age > 25 & age <= 30))
 
 gam4.out <- gam(nb2 ~ 1 + s(km, bs="cr", k = 7) + s(d, bs="cr", k = 3) + 
-                x6 + x7, family = poisson(link = log),
+                  x6 + x7, family = poisson(link = log),
                 data = data_train, scale = -1, gamma = 1)
 
 gam4.out$coefficients[2:3]
@@ -150,7 +130,7 @@ plot(gam4.out)
 
 
 gam5.out <- gam(nb2 ~ 1 + te(km, d, k = c(7, 3), bs = c("cr", "cr")) + 
-                x6 + x7, family = poisson(link = log), 
+                  x6 + x7, family = poisson(link = log), 
                 data = data_train, scale = -1, gamma = 1)
 
 gam5.out$coefficients[2:3]
